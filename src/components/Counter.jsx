c41e39bca78b730728af59e5edfc23f74bf6a9f5
akshaykanherkar7 <akshaykanherkar7@gmail.com>
2022-05-19 13:13:40 +0530

    Counter edited

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,26 +1,59 @@
 import React, { useState } from "react";
 import styles from "../App.module.css";
 
+var styleBox = {
+  // border: `1px solid red`,
+  width: `fit-content`,
+  margin: `auto`,
+  marginTop: `50px`,
+  padding: `15px`,
+  backgroundColor: `lightgray`,
+  borderRadius: `10px`,
+};
+
+var btnBox = {
+  display: `flex`,
+  gap: `30px`,
+  // border: `1px solid red`,
+  padding: `10px`,
+};
+
+var btnStyle = {
+  padding: `10px`,
+  borderRadius: `10px`,
+  backgroundColor: `teal`,
+  color: `white`,
+  border: `transparent`,
+};
 let Counter = () => {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Counter App</h1>
-      <h1 className={count % 2 === 1 ? `${styles.red}` : `${styles.green}`}>
-        {count}
-      </h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button
-        onClick={() => {
-          if (count > 0) {
-            setCount(count - 1);
-          }
-        }}
-      >
-        Decrement
-      </button>
-      <button onClick={() => setCount(count * 2)}>double</button>
+      <div style={styleBox}>
+        <h1>Counter App</h1>
+        <h1 className={count % 2 === 1 ? `${styles.red}` : `${styles.green}`}>
+          {count}
+        </h1>
+        <div style={btnBox}>
+          <button style={btnStyle} onClick={() => setCount(count + 1)}>
+            Increment
+          </button>
+          <button
+            style={btnStyle}
+            onClick={() => {
+              if (count > 0) {
+                setCount(count - 1);
+              }
+            }}
+          >
+            Decrement
+          </button>
+          <button style={btnStyle} onClick={() => setCount(count * 2)}>
+            double
+          </button>
+        </div>
+      </div>
     </>
   );
 };
